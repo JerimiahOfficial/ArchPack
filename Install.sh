e64a9ea951ee45ef26b259bbc21ca23d218c8cf2
Gage Lodba <38990407+JerimiahOfficial@users.noreply.github.com>
2023-01-13 14:05:18 -0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,28 +1,30 @@
 # !/bin/bash -e
-# Cd to the opt directory
-cd /opt
-
 # Check if multilib is installed
 if ! grep -q "\[multilib\]" /etc/pacman.conf; then
+    echo "Enabling multilib"
     sudo sed -i '/\[multilib\]/,/Include/s/^#//' /etc/pacman.conf
 fi
 
 # Make sure the system is up to date
+echo "Updating system"
 sudo pacman -Syu --noconfirm
 
 # Install all the packages that can be installed with pacman
-pacman=(bitwarden base-devel discord git p7zip steam ufw)
-
+pacman=(bitwarden discord git p7zip steam ufw)
 for i in "${pacman[@]}"; do
     echo "Installing $i"
-    sudo pacman -S --needed --noconfirm $i
+    sudo pacman -S --noconfirm $i >/dev/null
 done
 
 # Start and enable ufw
+echo "Enabling ufw"
 sudo systemctl enable ufw
 sudo systemctl start ufw
 
 # Installing yay
+echo "Installing yay"
+cd /opt
+
 sudo git clone https://aur.archlinux.org/yay.git
 sudo chown -R $USER:$USER ./yay
 
@@ -31,12 +33,12 @@ cd yay-git
 makepkg -si
 
 # Make sure the yay packages are up to date
+echo "Updating yay packages"
 sudo yay -Syu
 
 # Install all the packages that can be installed with yay
 yay=(github-desktop openrgb spotify visual-studio-code-bin)
-
 for i in "${yay[@]}"; do
     echo "Installing $i"
-    yay -S --noconfirm $i
+    yay -S --noconfirm $i >/dev/null
 done
