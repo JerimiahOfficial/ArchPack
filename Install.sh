eb1739afee8f4a8b6b09f7c00160abcdda1d7e42
Gage Lodba <38990407+JerimiahOfficial@users.noreply.github.com>
2023-01-12 23:13:01 -0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,17 +1,4 @@
 # !/bin/bash -e
-###############################################################################
-# This is a script to install all the software I need on a new arch system.
-#
-# The software that will be installed after running this script:
-# - Bitwarden
-# - Discord
-# - github-desktop
-# - openrgb
-# - p7zip
-# - Spotify
-# - Steam
-# - ufw
-# - VSCode
 
 # Cd to the opt directory
 cd /opt
