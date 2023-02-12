73397895951b1dc584ab36eccd9b74345ec853e1
Gage Lodba <38990407+JerimiahOfficial@users.noreply.github.com>
2023-02-12 14:42:39 -0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -5,11 +5,10 @@ if ! grep -q "\[multilib\]" /etc/pacman.conf; then
     exit 1
 fi
 
-echo "Updating system"
-sudo pacman -Syu --noconfirm
-
 echo "Installing pacman packages"
+sudo pacman -Syu --noconfirm >/dev/null
 pacman=(bitwarden discord git p7zip steam ufw)
+
 for i in "${pacman[@]}"; do
     echo "Installing $i"
     sudo pacman -S --noconfirm $i >/dev/null
@@ -29,12 +28,13 @@ cd yay
 
 makepkg -si
 
-echo "Updating yay packages"
-sudo yay -Syu
-
 echo "Installing yay packages"
+sudo yay -Syu --noconfirm >/dev/null
 yay=(github-desktop openrgb spotify visual-studio-code-bin)
+
 for i in "${yay[@]}"; do
     echo "Installing $i"
     yay -S --noconfirm $i >/dev/null
 done
+
+exit 0
