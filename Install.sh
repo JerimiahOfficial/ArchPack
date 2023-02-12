917075a240a527200b34185eb436f24e4291dbc0
Gage Lodba <38990407+JerimiahOfficial@users.noreply.github.com>
2023-02-11 23:46:22 -0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -9,36 +9,63 @@ fi
 echo "Updating system"
 sudo pacman -Syu --noconfirm
 
-# Install all the packages that can be installed with pacman
-pacman=(bitwarden discord git p7zip steam ufw)
-for i in "${pacman[@]}"; do
-    echo "Installing $i"
-    sudo pacman -S --noconfirm $i >/dev/null
-done
+# Install ufw
+echo "Installing ufw"
+sudo pacman -S ufw --noconfirm
 
-# Start and enable ufw
+# Enable ufw
 echo "Enabling ufw"
 sudo systemctl enable ufw
 sudo systemctl start ufw
 
-# Installing yay
-echo "Installing yay"
-cd /opt
+# Install nix package manager
+echo "Installing nix"
+pacman -S nix --noconfirm
 
-sudo git clone https://aur.archlinux.org/yay.git
-sudo chown -R $USER:$USER ./yay
+# Launch nix daemon
+echo "Launching nix daemon"
+sudo systemctl enable nix-daemon
+sudo systemctl start nix-daemon
 
-cd yay
+# Add current user to nix group
+echo "Adding current user to nix group"
+sudo usermod -a -G nix-users $USER
+sudo usermod -a -G nix-bld $USER
 
-makepkg -si
+# Run nix setup
+echo "Running nix setup"
+nix-env --install
 
-# Make sure the yay packages are up to date
-echo "Updating yay packages"
-sudo yay -Syu
+# Add channels
+echo "Adding channels"
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+nix-channel --update
 
-# Install all the packages that can be installed with yay
-yay=(github-desktop openrgb spotify vscodium-bin)
-for i in "${yay[@]}"; do
+# Install software
+echo "Installing software"
+
+# List of packages to install
+# Bitwarden
+# Discord
+# github-desktop
+# openrgb
+# p7zip
+# Spotify
+# Steam
+# vscodium
+list=(
+    bitwarden
+    discord
+    github-desktop
+    openrgb
+    p7zip
+    spotify
+    steam
+    vscodium
+)
+
+# Install packages
+for i in "${list[@]}"; do
     echo "Installing $i"
-    yay -S --noconfirm $i >/dev/null
+    nix-env -iA nixpkgs.$i
 done
