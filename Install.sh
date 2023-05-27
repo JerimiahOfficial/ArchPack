ca1aded47e05249c43570d76321f760d77fd12b8
Gage Lodba <JerimiahOfficial@gmail.com>
2023-05-26 21:15:32 -0400

    Installing sddm that was missed.

--- Install.sh
+++ Install.sh
@@ -45,7 +45,8 @@ yay -S github-desktop-bin vscodium-bin minecraft-launcher
 
 echo "Installing themes"
 sudo git clone https://github.com/vinceliuice/Orchis-kde.git
-(cd Orchis-kde && bash install.sh)
+(cd ./Orchis-kde && bash install.sh)
+(cd ./Orchis-kde/sddm && bash install.sh)
 
 sudo git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git
 (cd Tela-circle-icon-theme && bash install.sh)
