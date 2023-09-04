cf58c3faa7c9f44401550cc8b391cdaa73b13a87
Gage Lodba <JerimiahOfficial@gmail.com>
2023-09-04 11:27:15 -0400

    Update Chroot.sh

--- Chroot.sh
+++ Chroot.sh
@@ -1,3 +1,7 @@
+# Clear and print
+clear
+echo "=====| Chroot |====="
+
 # Timezone
 ln -sf /usr/share/zoneinfo/America/Toronto /etc/localtime
 hwclock --systohc
@@ -21,6 +25,7 @@ sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 
 # Add user
 useradd jerimiah -m -G wheel,optical,disk,storage
+passwd jerimiah 1234
 
 # Bootloader
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
