d9272776124187e455f99c3919efccb8bf7d93cd
Gage Lodba <JerimiahOfficial@gmail.com>
2023-09-07 13:41:41 -0400

    Update Chroot.sh

--- Chroot.sh
+++ Chroot.sh
@@ -13,12 +13,13 @@ hwclock --systohc
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 
 # Set env variables
-export LANGUAGE=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-
-locale-gen
-
+# export LANGUAGE=en_US.UTF-8
+# export LANG=en_US.UTF-8
+# export LC_ALL=en_US.UTF-8
+# LANGUAGE=en_US.UTF-8
+# LANG=en_US.UTF-8
+# LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
 loadkeys us
 
 # Network configuration
