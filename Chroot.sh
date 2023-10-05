a4645558f3fbb6662148594b8d36fc1fe64774e9
Gage Lodba <JerimiahOfficial@gmail.com>
2023-10-04 22:48:09 -0400

    Update Chroot.sh

--- Chroot.sh
+++ Chroot.sh
@@ -2,10 +2,13 @@
 set -e
 
 # Localization
-echo "en_US.UTF-8 UTF-8" >/etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" >/etc/locale.conf
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LC_MESSAGES=en_US.UTF-8
 export LANG=en_US.UTF-8
+
+echo "en_US.UTF-8 UTF-8" >>/etc/locale.gen
+locale-gen
 # echo "KEYMAP=us" >>/etc/vconsole.conf
 
 # Timezone
