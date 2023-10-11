ced7f6aa1bdaf6c0895ac895de903c038ab1ef07
Gage Lodba <JerimiahOfficial@gmail.com>
2023-10-10 21:53:52 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,9 +1,5 @@
 # !/bin/bash -e
 
-# Download chroot script for later
-curl -s https://raw.githubusercontent.com/JerimiahOfficial/ArchPack/main/Chroot.sh >>/mnt/Chroot.sh
-chmod +x /mnt/Chroot.sh
-
 # Sync the system clock
 timedatectl
 
@@ -31,9 +27,8 @@ mount --mkdir /dev/sda1 /mnt/boot
 swapon /dev/sda2
 mount --mkdir /dev/sda3 /mnt
 
-# Get the best mirrors
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup >/etc/pacman.d/mirrorlist
+# Get mirror list
+curl -s 'https://archlinux.org/mirrorlist/?country=CA&protocol=http&protocol=https&ip_version=4&ip_version=6' >/etc/pacman.d/mirrorlist
 
 # Installing base system
 pacstrap -K /mnt base linux linux-firmware
@@ -41,9 +36,12 @@ pacstrap -K /mnt base linux linux-firmware
 # Generating fstab
 genfstab -U -p /mnt >>/mnt/etc/fstab
 
+# Download chroot script
+curl -s https://raw.githubusercontent.com/JerimiahOfficial/ArchPack/main/Chroot.sh >/mnt/Chroot.sh
+chmod +x /mnt/Chroot.sh
+
 # Chroot
-arch-chroot /mnt
-./Chroot.sh
+arch-chroot /mnt /bin/bash /Chroot.sh
 
 # unmount all
 # umount -R /mnt
